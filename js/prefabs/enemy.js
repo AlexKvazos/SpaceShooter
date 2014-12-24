13b95f6de0dee46d30d2a08d285fdb2569b2b24d
AlexKvazos <contact@alexkvazos.com>
2014-12-24 10:45:53 -0600

    Fixed enemy rotor and movement

--- js/prefabs/enemy.js
+++ js/prefabs/enemy.js
@@ -13,6 +13,10 @@ SpaceShooter.Prefabs.Enemy.prototype = {
 
     SpaceShooter.Game.load.image('enemy1', 'assets/enemy.png');
     SpaceShooter.Game.load.image('enemy2', 'assets/enemy2.png');
+
+    SpaceShooter.Game.load.image('enemy.blue.body', 'assets/enemies/blue_body.png');
+    SpaceShooter.Game.load.image('enemy.blue.rotor', 'assets/enemies/blue_rotor.png');
+
     SpaceShooter.Game.load.spritesheet('enemyParticle', 'assets/enemyParticle.png', 2, 2, 7);
     SpaceShooter.Game.load.audio('impact', 'assets/impact.mp3');
 
@@ -20,7 +24,7 @@ SpaceShooter.Prefabs.Enemy.prototype = {
 
   create: function() {
 
-    this.createGreenEnemies();
+    this.createBlueEnemies();
 
     // create enemy impact sound
     this.state.enemyImpact = SpaceShooter.Game.add.sound('impact');
@@ -50,45 +54,49 @@ SpaceShooter.Prefabs.Enemy.prototype = {
     });
 
     // update player movement
-    this.state.greenEnemies.forEachAlive(this.move, this);
+    this.state.blueEnemies.forEachAlive(this.move, this);
 
     // get a dead instance
-    var greenEnemy = this.state.greenEnemies.getFirstDead();
+    var greenEnemy = this.state.blueEnemies.getFirstDead();
     if (greenEnemy) {greenEnemy.reset(SpaceShooter.Game.rnd.integerInRange(100, 460), 100, 10)};
 
   },
 
   move: function(enemy) {
-    enemy.body.angle += 6;
+    enemy.children[0].angle += 10;
     this.accelerateToObject(enemy, this.state.player, enemy.speed);
   },
 
   accelerateToObject: function(obj1, obj2, speed) {
 
     var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
+    obj1.body.rotation = angle + SpaceShooter.Game.math.degToRad(90);
     obj1.body.x += Math.cos(angle) * speed;
     obj1.body.y += Math.sin(angle) * speed;
 
   },
 
-  createGreenEnemies: function() {
+  createBlueEnemies: function() {
 
     // green enemies group
-    this.state.greenEnemies = SpaceShooter.Game.add.group();
+    this.state.blueEnemies = SpaceShooter.Game.add.group();
 
     // create 10 green enemies
     for (var i = 0; i < 1; i++) {
-      var enemy = this.state.greenEnemies.create(100, 100, 'enemy2', 0, true);
+      var enemy = this.state.blueEnemies.create(100, 100, 'enemy.blue.body', 0, true);
       SpaceShooter.Game.physics.p2.enable(enemy, false);
       enemy.body.setCircle(60);
       enemy.body.kinematic = true;
       enemy.body.collideWorldBounds = false;
       enemy.body.setCollisionGroup(this.state.enemyCollisionGroup);
       enemy.body.collides(this.state.bulletCollisionGroup);
-      enemy.anchor.setTo(0.5, 0.6);
+      enemy.anchor.setTo(0.5, 0.5);
       enemy.health = 10;
       enemy.speed = 1.3;
       enemy.type = 0;
+      var enemy_rotor = SpaceShooter.Game.add.sprite(0, 0, 'enemy.blue.rotor');
+      enemy_rotor.anchor.setTo(0.5 ,0.5);
+      enemy.addChild(enemy_rotor);
     };
 
   },
