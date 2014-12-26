2d51828e29bd62c7540aeafacd76b933b23bb19d
AlexKvazos <contact@alexkvazos.com>
2014-12-26 01:08:27 -0600

    Added tilt to intro guide

--- js/prefabs/guide.js
+++ js/prefabs/guide.js
@@ -15,8 +15,8 @@ SpaceShooter.Prefabs.Guide.prototype = {
      * Guide 1
      * "Touch to Shoot"
      */
-    var g1 = SpaceShooter.Game.add.text(SpaceShooter.Game.world.centerX, SpaceShooter.Game.world.height, 'TOUCH TO SHOOT', {
-      font: '60px Roboto Condensed Light', fill: '#349fff'
+    var g1 = SpaceShooter.Game.add.text(SpaceShooter.Game.world.centerX, SpaceShooter.Game.world.height, 'TOUCH TO SHOOT\nTILT TO MOVE', {
+      font: '50px Roboto Condensed Light', fill: '#349fff', align: 'center'
     });
     g1.anchor.setTo(0.5, 0);
     g1.z = -9999;
@@ -24,7 +24,7 @@ SpaceShooter.Prefabs.Guide.prototype = {
 
     SpaceShooter.Game.time.events.add(1000, function() {
       SpaceShooter.Game.add.tween(g1).to({y: 500, alpha: 1}, 1200, Phaser.Easing.Cubic.Out, true);
-      SpaceShooter.Game.time.events.add(1700, function() {
+      SpaceShooter.Game.time.events.add(1900, function() {
         SpaceShooter.Game.add.tween(g1).to({y: -g1.height, alpha: 0}, 900, Phaser.Easing.Cubic.In, true);
       }, this);
     }, this);
